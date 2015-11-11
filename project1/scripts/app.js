0d27197306706a4801740c9439615ef0099de736
Jeff Steed <steed.jeff@gmail.com>
2015-11-11 11:32:59 -0600

    Added dice selection functionality.

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -4,41 +4,78 @@ window.onload = function() {
   document.getElementById('roll-dice').addEventListener('click', function() {
     diceRoll();
   });
+
+  setDiceEventListeners();
 };
 
-var one = 'http://www.booksandwands.com/images/ChineseOne.bmp';
-var two = 'http://www.booksandwands.com/images/ChineseTwo.bmp';
-var three = 'http://www.booksandwands.com/images/ChineseThree.bmp';
-var four = 'http://www.booksandwands.com/images/ChineseFour.bmp';
-var five = 'http://www.booksandwands.com/images/ChineseFive.bmp';
+var diceImgs = {
+  one: 'http://www.booksandwands.com/images/ChineseOne.bmp',
+  two: 'http://www.booksandwands.com/images/ChineseTwo.bmp',
+  three: 'http://www.booksandwands.com/images/ChineseThree.bmp',
+  four: 'http://www.booksandwands.com/images/ChineseFour.bmp',
+  five: 'http://www.booksandwands.com/images/ChineseFive.bmp'
+}
+
+// Function to identify selected dice
+function setDiceEventListeners() {
+  console.log("Event listeners added.");
+  var diceArray = ['dice1', 'dice2', 'dice3', 'dice4', 'dice5'];
+  for (var eachDie in diceArray) {
+    var die = dice[diceArray[eachDie]];
+    console.log(die);
+    die.addEventListener('click', function() {
+      var myDie = this;
+      if (myDie.classList.contains('selected')) {
+        myDie.classList.remove('selected');
+      } else {
+        myDie.classList.add('selected');
+      }
+    });
+  }
+};
+
+
+var dice = {
+  dice1: document.getElementById('dice1'),
+  dice2: document.getElementById('dice2'),
+  dice3: document.getElementById('dice3'),
+  dice4: document.getElementById('dice4'),
+  dice5: document.getElementById('dice5')
+};
 
 
 function diceRoll() {
   console.log("diceRoll ran");
-  document.getElementById('diceOne').style.backgroundImage = "url('" + roll() + "')";
-  document.getElementById('diceTwo').style.backgroundImage = "url('" + roll() + "')";
-  document.getElementById('diceThree').style.backgroundImage = "url('" + roll() + "')";
-  document.getElementById('diceFour').style.backgroundImage = "url('" + roll() + "')";
-  document.getElementById('diceFive').style.backgroundImage = "url('" + roll() + "')";
+  var diceArray = ['dice1', 'dice2', 'dice3', 'dice4', 'dice5'];
+  var diceToRoll = [];
+  for (var eachDie in diceArray) {
+    var die = dice[diceArray[eachDie]];
+    if (!die.classList.contains('selected')) {
+      diceToRoll.push(die);
+    }
+  }
+  for (var i = 0; i < diceToRoll.length; i++) {
+    diceToRoll[i].style.backgroundImage = "url('" + roll() + "')";
+  }
 };
 
 function roll() {
   var num = Math.floor(Math.random() * 5 + 1);
   switch (num) {
     case 1:
-      return one;
+      return diceImgs.one;
       break;
     case 2:
-      return two;
+      return diceImgs.two;
       break;
     case 3:
-      return three;
+      return diceImgs.three;
       break;
     case 4:
-      return four;
+      return diceImgs.four;
       break;
     case 5:
-      return five;
+      return diceImgs.five;
       break;
     default:
       console.log("Error: the number was not between 1 and 5.");
